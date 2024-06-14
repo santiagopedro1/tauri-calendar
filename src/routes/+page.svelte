2a96bc0d80ec17ec9a454e3ed085840479e28f52
Pedro Santiago <pedrossantiago1@gmail.com>
2024-06-14 17:03:23 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Calendar from '$lib/components/Calendar.svelte';
+	import { getLocalTimeZone, today } from '@internationalized/date';
+
+	let value = today(getLocalTimeZone());
+</script>
+
+<Calendar
+	{value}
+	class="w-full"
+/>
